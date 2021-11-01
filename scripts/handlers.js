2acdde8ec530f38d18f89b68b4592cf04dab6068
Sergey Lowly <87949654+L0w1y@users.noreply.github.com>
2021-11-01 14:42:51 +0700

    Delete scripts directory

--- scripts/handlers.js
+++
@@ -1,1 +0,0 @@
-gwd.auto_AdcardClick=function(event){gwd.actions.events.setInlineStyle("adcard","transition: all 0.5s ease-in-out; opacity:  0;")}
--- scripts/loc_ru.js
+++
@@ -1,5 +0,0 @@
- var userLang = navigator.language || navigator.userLanguage;
-            if (userLang !== "en") {
-              window.location.href = 'https://l0w1y.github.io/ru/main.html';
-//                 window.location.href = "/ru/main.html";
-            }
