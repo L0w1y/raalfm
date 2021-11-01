88b31fa1872dd511b4685420a17bf07392383ca5
Sergey Lowly <87949654+L0w1y@users.noreply.github.com>
2021-11-01 11:51:16 +0700

    Updated location link after localization
    Updated location page and localization script

--- scripts/loc_ru.js
+++ scripts/loc_ru.js
@@ -1,4 +1,5 @@
  var userLang = navigator.language || navigator.userLanguage;
             if (userLang !== "en") {
-                window.location.href = "/ru/main.html";
+              window.location.href = 'https://l0w1y.github.io/ru/main.html';
+//                 window.location.href = "/ru/main.html";
             }
