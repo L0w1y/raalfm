4701a3a229c54793f72be9d578342abc32eaa4ce
Sergey Lowly <87949654+L0w1y@users.noreply.github.com>
2021-11-01 11:49:34 +0700

    Update location after time rigging
    Updated link to localized page

--- scripts/loc_en.js
+++ scripts/loc_en.js
@@ -1,6 +1,7 @@
  var userLang = navigator.language || navigator.userLanguage;
             if (userLang !== "ru") {
-                window.location.href = "/en/main.html";
+              window.location.href = 'https://l0w1y.github.io/en/main.html';
+//                 window.location.href = "/en/main.html";
             }
 // if (userLang = en) {
 // 	document.body.innerHTML = document.body.innerHTML.replace('упс, сайт еще не работает, но скоро заработает. Если вам нужны последние новости проекта, нажмите на карточку.', 'Uupps, The site is not working yet, but it will work soon. If you need the latest project news click on card.');
